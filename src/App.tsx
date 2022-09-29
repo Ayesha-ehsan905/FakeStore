15560cca18ceccc142936f8739157cbd4d923176
Ayesha-ehsan905 <109783985+Ayesha-ehsan905@users.noreply.github.com>
2022-09-29 17:45:24 +0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import {
   Navigate,
 } from "react-router-dom";
 import React, { Suspense, useContext, useState } from "react";
-import Login from "./component/login/Login";
-import Products from "./component/products/Products";
+import Login from "./views/login/Login";
+import Products from "./views/products/Products";
 import AuthProvider, { AuthContext } from "./context/AuthContext";
-const LoginIndex = React.lazy(() => import("./component/login/Login"));
-const ProductIndex = React.lazy(() => import("./component/products/Products"));
+const LoginIndex = React.lazy(() => import("./views/login/Login"));
+const ProductIndex = React.lazy(() => import("./views/products/Products"));
 
 function LogoutCheck({ children }: { children: JSX.Element }) {
   let auth = useContext(AuthContext);
--- src/utlis/Schema.ts
+++ src/utlis/Schema.ts
@@ -1,7 +1,9 @@
 import * as yup from "yup";
 const Schema = yup.object().shape({
-  Password: yup.string().required().max(6).min(6),
-  UserName: yup.string().required().max(8).min(8),
+  Password: yup.string().required(),
+  // .max(6).min(6),
+  UserName: yup.string().required(),
+  // .max(8).min(8),
 });
 
 export default Schema;
